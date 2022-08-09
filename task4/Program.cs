02fa2b0192affeae765344ab978d599bab061376
Andrey <ixpsychosocial@gmail.com>
2022-08-09 22:43:21 +0300

    add task 4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 3 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
